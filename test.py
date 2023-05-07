cd14b8d350d405839948d13e556ea7b00a878e6f
Dmitry <tdmitryny@gmail.com>
2023-05-07 10:38:29 -0400

    Statement

--- test.py
+++ test.py
@@ -1,23 +1,7 @@
-bill = input("What was the total bill? $")
+print("Welcome to rollercoaster")
+height = int(input("what's you height?"))
 
-float_number = float(bill)
-
-ten = 10 / 100
-twelve = 12 / 100
-fifteen = 15 /100
-
-percentage = input(f"What percentage tip would like to give? 10, 12, or 15?")
-
-total_per = float(percentage) / 100
-number_percent  = float_number * total_per
-
-
-total_number = float_number + number_percent
-
-split = input("How many people to split?")
-
-total = total_number / int(split)
-
-
-print(f"Each person should pay: ${total // 1}")
-
+if height >= 120:
+  print("Yoy are good")
+else:
+  print("Too short")
