607301e473a07db4bf4dbf09a9c40837b711fc2b
Dmitry <tdmitryny@gmail.com>
2023-05-07 16:35:44 -0400

    BMI calcualtion

--- bmi.py
+++ bmi.py
@@ -1,5 +1,19 @@
 height = float(input("What is your height in meters?"))
 weight = float(input("What is your weight in kg:?"))
 
-bmi = weight / height ** 2
+bmi = int(weight) / float(height) * 2
 
+if bmi <= 18:
+    print(f"Your BMI {bmi * 100}, you are in shape")
+elif bmi >= 18.5:
+    if bmi < 25:
+      print(f"Your BMI {bmi}, you have normal weight")
+elif bmi > 25:
+    if bmi < 30:
+      print(f"Your BMI {bmi}, you are overweight")
+elif bmi > 30:
+    if bmi < 35:
+      print(f"Your BMI {bmi}, you are obese") 
+elif bmi > 35:
+    print(f"Your BMI {bmi}, clinical obese")   
+    
