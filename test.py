9e1373c466b2c29e92ed39a39aedca2486bbc900
Dmitry <tdmitryny@gmail.com>
2023-05-07 10:48:29 -0400

    State

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 print("Welcome to rollercoaster")
 height = int(input("what's you height?"))
+age = int(input("What's your age?"))
 
 if height >= 120:
-  print("Yoy are good")
+  if age >= 18:
+    print("Yoy are good")
+  else:
+    print("Too young")
 else:
   print("Too short")
