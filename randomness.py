396f8e9a28bbe6c600fc9804eba849436a5921c5
Dmitry <tdmitryny@gmail.com>
2023-05-08 09:39:14 -0400

    Random

--- randomness.py
+++ randomness.py
@@ -1,8 +1,16 @@
 import random
 
 
+#dice = input("Heads or Tails?")
+
+name_string = input("Give me everybody's name seperated by a comma")
+names = name_string.split(", ")
+
+items = len(names)
+
+person = random.randint(0,items -1)
+final_name = names[person]
 
 
-print(random.randint(100,233))
-random_float = random.random() * 5
-print(random_float)
+print(f"{final_name} is going to buy meal today")
+#Vasya, Petya, Mosya, Kosya, Rvosya
